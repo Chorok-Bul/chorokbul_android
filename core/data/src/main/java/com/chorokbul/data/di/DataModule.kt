e83075926a3176b0199da947bf0ccc598df60d15
SANDY <nnal0256@naver.com>
2024-04-30 00:14:09 +0900

    [feat] InsertRawDataRepository, Hilt 모듈 생성

---
+++ core/data/src/main/java/com/chorokbul/data/di/DataModule.kt
@@ -0,0 +1,20 @@
+package com.chorokbul.data.di
+
+import com.chorokbul.data.repository.AppPrefsRepository
+import com.chorokbul.data.repository.InsertRawDataRepository
+import com.chorokbul.data.repository.impl.InsertRawDataRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+internal abstract class DataModule {
+
+    @Binds
+    abstract fun bindsInsertRawDataRepository(
+        impl: InsertRawDataRepositoryImpl
+    ): InsertRawDataRepository
+
+}
