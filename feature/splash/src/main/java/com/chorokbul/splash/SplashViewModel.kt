1da9cb36dcc0f638e5eb3ece8b8ed1045771bb7e
SANDY <nnal0256@naver.com>
2024-05-03 14:17:13 +0900

    [fix] SplashViewModel : 데이터 다운로드 상태 확인 로직 추가

--- feature/splash/src/main/java/com/chorokbul/splash/SplashViewModel.kt
+++ feature/splash/src/main/java/com/chorokbul/splash/SplashViewModel.kt
@@ -4,25 +4,33 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.chorokbul.domain.GetIsDataDownloadedUseCase
 import com.chorokbul.domain.InsertRawDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val insertRawDataUseCase: InsertRawDataUseCase
+    private val insertRawDataUseCase: InsertRawDataUseCase,
+    private val getIsDataDownloadedUseCase: GetIsDataDownloadedUseCase
 ) : ViewModel() {
 
     private val _completed = mutableStateOf(false)
     val complete: State<Boolean> get() = _completed
 
     init {
-        viewModelScope.launch {
-            async(Dispatchers.IO) { insertRawDataUseCase() }.await()
-            _completed.value = true
-        }
+        getIsDataDownloadedUseCase.getIsSoundSignalDownloaded().onEach { isDownloaded ->
+            _completed.value = isDownloaded
+            if (!isDownloaded) downloadSoundSignal()
+        }.launchIn(viewModelScope)
+    }
+
+    private fun downloadSoundSignal() = viewModelScope.launch {
+        async(Dispatchers.IO) { insertRawDataUseCase() }.await()
     }
 }
